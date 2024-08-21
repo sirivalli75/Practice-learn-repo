068aa169e51e22681d6d5c761d4d5d29d6cc030a
sirivalli75 <158605703+sirivalli75@users.noreply.github.com>
2024-08-21 22:46:49 +0100

    add event to download resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  host: {ngSkipHydration : 'true'}
 })
 export class AppComponent {
+  title= 'Cascade';
+ header() {
+  console.log('clicked');
+ }
+ 
+downloadResume() {
+  const link = document.createElement('a');
+    link.href = 'assets/resume/resume.pdf';
+    link.download = 'Siri_Pechetti_Resume.pdf';
+    link.click();
 }
+  }
+
